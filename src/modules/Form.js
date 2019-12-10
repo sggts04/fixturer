12005d27a3affb3ffcd54bc1bd2db7ae96c46644
Shreyas Gupta <technology.shreyas@gmail.com>
2019-12-10 16:38:33 +0530

    Added team names input

--- src/modules/Form.js
+++ src/modules/Form.js
@@ -6,19 +6,21 @@ class Form extends React.Component {
         super(props);
 
         this.state = {
-            tname: '',
+            toname: '',
             tnum: '',
+            tnames: [],
             stage: 0
         };
 
-        this.updateName = this.updateName.bind(this);
+        this.updateToName = this.updateToName.bind(this);
         this.updateNum = this.updateNum.bind(this);
+        this.goBack = this.goBack.bind(this);
         this.submit = this.submit.bind(this);
     }
 
-    updateName(e) {
+    updateToName(e) {
         this.setState({
-            tname: e.target.value
+            toname: e.target.value
         });
     }
 
@@ -30,13 +32,24 @@ class Form extends React.Component {
         }
     }
 
+    goBack() {
+        this.setState({
+            stage: 0
+        });
+    }
+
     submit() {
         if(!( Number.isInteger(Math.log(this.state.tnum)/Math.log(2)) && Number(this.state.tnum) !== 1)) {
             alert("Number of teams must be a power of 2!");
         }
         else {
+            var tnamestemp = this.state.tnames;
+            for(var i=0; i<this.state.tnum; i++) {
+                tnamestemp.push('');
+            }
             this.setState({
-                stage: 1
+                stage: 1,
+                tnames: tnamestemp
             });
         }
     }
@@ -46,9 +59,9 @@ class Form extends React.Component {
             (this.state.stage===0)?(
                 
                 <div className="Form">
-                    <h2>Fixturer</h2>
-                    <p>Fixturer is a Web App that generates single-elimination style fixtures.</p>
-                    <input type="text" name="tname" placeholder="Tournament Name" value={this.state.tname} onChange={this.updateName}></input>
+                    <h2 className="title">Fixturer</h2>
+                    <p className="description">Fixturer is a Web App that generates single-elimination style fixtures.</p>
+                    <input type="text" name="toname" placeholder="Tournament Name" value={this.state.toname} onChange={this.updateToName}></input>
                     <br/><br/>
                     <input type="text" name="tnum" placeholder="Number of Teams" value={this.state.tnum} onChange={this.updateNum}></input>
                     <br/>
@@ -56,9 +69,17 @@ class Form extends React.Component {
                 </div>
                 
                 ):(
-
-                <p>Enter Team Names</p>
-            
+                <div className="Form">
+                    <p className="title2">Enter Team Names</p>
+                    {this.state.tnames.map((name, i) => (
+                        <div>
+                            <input type="text" key={i} placeholder={i+1}></input>
+                            <br/><br/>
+                        </div>
+                    ))}
+                    <button className="back" onClick={this.goBack}>BACK</button>
+                    <button className="names-submit">GO</button>
+                </div>
                 )
             );
     }
